6c52be68f8438154be00a022435cda90a0b633b3
SoFa325 <sofyabel@inbox.ru>
2023-05-14 17:19:15 +0300

    наброски веб страниц(гл и нов препод)

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
     "database/sql"
     "fmt"
-    "e-university-sample/dataBase"
+    "net/http"
+    "html/template"
+    //"e-university-sample/dataBase"
     _ "github.com/lib/pq"
 )
 
@@ -11,36 +13,74 @@ const (
     host     = "localhost"
     port     = 5432
     user     = "postgres"
-    password = "ub7u3nAntu"
-    // password = "postgres"
+    password = "postgres"
     //password = "ub7u3nAntu"
     dbname   = "Euniversity"
 )
 
-func main() {
+func index(w http.ResponseWriter, r *http.Request){
+    t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+
+    if err != nil {
+        fmt.Fprintf(w, err.Error())
+    }
+
+    t.ExecuteTemplate(w, "index", nil)
+}
+
+func new_teacher(w http.ResponseWriter, r *http.Request){
+    t, err := template.ParseFiles("templates/new_teacher.html", "templates/header.html", "templates/footer.html")
+
+    if err != nil {
+        fmt.Fprintf(w, err.Error())
+    }
+
+    t.ExecuteTemplate(w, "new_teacher", nil)
+}
+
+func save_teacher(w http.ResponseWriter, r *http.Request){
+    name := r.FormValue("name")
+    surname := r.FormValue("surname")
+    patronymic := r.FormValue("patronymic")
+    email := r.FormValue("email")
+    login := r.FormValue("login")
+
     psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-        "password=%s dbname=%s sslmode=disable",
-        host, port, user, password, dbname)
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
     db, err := sql.Open("postgres", psqlInfo)
     if err != nil {
         panic(err)
     }
     defer db.Close()
-  
+
     err = db.Ping()
     if err != nil {
         panic(err)
     }
-  
+
     fmt.Printf("Successfully connected!\n\n")
 
-
-    res, err := db.Query("select * from StudentGroup")
+    res, err := db.Query(fmt.Sprintf("INSERT INTO Teacher(TeacherID, user_id, TeacherName, Surname,Patronymic, Email) Values (gen_random_uuid(),(SELECT UserID From Users Where Login = '%s') ,'%s', '%s','%s',  '%s')",login, name, surname, patronymic, email ))
     if err != nil {
         panic(err)
     }
     defer res.Close()
-    studentGroups := []dataBase.StudentGroup{}
+    http.Redirect(w,r,"/", http.StatusSeeOther)
+}
+
+func handleFunc() {
+    http.HandleFunc("/", index)
+    http.HandleFunc("/new_teacher", new_teacher)
+    http.HandleFunc("/save_teacher", save_teacher)
+    http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+    handleFunc()
+
+
+    /*studentGroups := []dataBase.StudentGroup{}
     for res.Next(){
         g := dataBase.StudentGroup{}
         err := res.Scan(&g.Id, &g.Name, &g.YearOfAdm, &g.Course, &g.Amount)
@@ -54,5 +94,5 @@ func main() {
     fmt.Println("\nstudentGroups names:")
     for _, g := range studentGroups{
         fmt.Println(g.Get_name())
-    }
+    }*/
 }
